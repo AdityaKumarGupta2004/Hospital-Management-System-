7d04d9aa17e27aca1bf66fa14d0b5e55a11da388
Aditya Kumar Gupta <adityakumargupta45321@gmail.com>
2026-01-21 14:54:09 +0530

    21st Jan 2026

---
+++ src/test/java/com/learningJava/Hospital/Management/System/HospitalManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learningJava.Hospital.Management.System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
